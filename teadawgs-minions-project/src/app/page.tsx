32ec3353ac1d17f046643a09c2584bc9fe8e88ef
Firedynex <58797454+Firedynex@users.noreply.github.com>
2025-04-11 13:03:47 -0400

    Added conditional rendering

--- teadawgs-minions-project/src/app/page.tsx
+++ teadawgs-minions-project/src/app/page.tsx
@@ -1,10 +1,14 @@
+"use client"
 import Sidebar from "./ui-elements/sidebar";
 import Header from "./ui-elements/header";
+import { useState } from "react";
 
 export default function Home() {
+  // Simulate authentication state
+  const [authenticated, setAuthenticated] = useState(false);
   return (
     <>
-      <Header />
+      <Header authenticated = {authenticated}/>
       <Sidebar />
     </>
   );
--- teadawgs-minions-project/src/app/ui-elements/header.tsx
+++ teadawgs-minions-project/src/app/ui-elements/header.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { Lilita_One, Roboto } from "next/font/google";
 import { useState } from "react";
+import Link from "next/link";
 
 const lilita_One = Lilita_One({
     subsets: ["latin"],
@@ -13,8 +14,11 @@ const roboto = Roboto({
     weight: "400"
 })
 
-export default function Header() {
-    const [loggedIn, setLoggedIn] = useState(true);
+export default function Header({authenticated}: {authenticated: boolean}) {
+    const [loggedIn, setLoggedIn] = useState(authenticated);
+    function handleLogout() {
+        setLoggedIn(!loggedIn);
+    }
 
     return (
         <div className="flex justify-center items-center bg-red-400 w-full rounded-2xl h-20 space-x-2">
@@ -38,9 +42,9 @@ export default function Header() {
                     className="top-4 right-4 cursor-pointer"
                     priority
                 />
-                <button onClick={() => setLoggedIn(!loggedIn)} className="flex items-center justify-between border-solid border-2 border-black rounded-full bg-black p-2 hover:bg-gray-700 transition duration-300 mr-4 cursor-pointer">
+                <button onClick={handleLogout} className="flex items-center justify-between border-solid border-2 border-black rounded-full bg-black p-2 hover:bg-gray-700 transition duration-300 mr-4 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#e3e3e3"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
-                    <span className={`${roboto.className} text-2xl`}>Logout</span>
+                    <span className={`${roboto.className} text-2xl`}>{loggedIn ? "Logout" : "Login"}</span>
                 </button>
             </div>
             
