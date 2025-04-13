423d3b9949a67a284a10e46a83da5a8bf0f48f3c
peterbenbui <peterbui420@gmail.com>
2025-04-13 17:37:55 -0400

    restart

--- teadawgs-minions-project/src/app/add-post/page.tsx
+++ teadawgs-minions-project/src/app/add-post/page.tsx
@@ -1,7 +1,7 @@
 export default function AddPost(){
     return(
         <div className="background-color bg-gray-100 p-6">
-            <h1 className="text1 font-bold mb-6">Create New Post</h1>
+            <h1 className="text1 font-bold mb-6">hello</h1>
         </div>
     )
 }
