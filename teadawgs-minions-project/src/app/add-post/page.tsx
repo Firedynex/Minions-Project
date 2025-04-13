2b058e90ae8580d114ec217d92a98ae1d29a972b
peterbenbui <peterbui420@gmail.com>
2025-04-13 15:36:37 -0400

    gray background

--- teadawgs-minions-project/src/app/add-post/page.tsx
+++ teadawgs-minions-project/src/app/add-post/page.tsx
@@ -1,4 +1,7 @@
 export default function AddPost(){
-    //testing again
-    
+    return(
+        <div className="background-color bg-gray-100 p-6">
+            <h1 className="text1 font-bold mb-6">Create New Post</h1>
+        </div>
+    )
 }
