bb6efc52afaba64e8ae421893ff2fd1130638597
peterbenbui <peterbui420@gmail.com>
2025-04-13 19:30:11 -0400

    draft and message button

--- teadawgs-minions-project/src/app/add-post/page.tsx
+++ teadawgs-minions-project/src/app/add-post/page.tsx
@@ -100,7 +100,26 @@ export default function AddPost(){
                 </div>
             </div>
         </div>
-        
+        <div className="absolute bottom-8 w-full">
+            <div className="flex justify-between gap-30"
+                style={{
+                    marginLeft: 'calc(40%)',
+                    marginRight: '30px'
+                }}>
+                    <button className="flex justify-center items-center gap-2 bg-[#757575] px-8 py-3 rounded-lg hover:bg-[#858585] transition-colors w-[40%]">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#D8D8D8" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
+                        </svg>
+                        <span className="text-[#D8D8D8] text-lg">Drafts</span>
+                    </button>
+                    <button className="flex justify-center items-center gap-2 bg-[#E54747] px-8 py-3 rounded-lg hover:bg-[#F55757] transition-colors w-[40%] ml-2" style={{marginRight: 'auto'}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#D8D8D8" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
+                        </svg>
+                        <span className="text-[#D8D8D8] text-lg">Post</span>
+                    </button>
+                </div>
+        </div>
 </div>
     )
 }
