35106b5d5993a2c262f655ea16eaa6d640092b2f
peterbenbui <peterbui420@gmail.com>
2025-04-13 23:56:00 -0400

    add components

--- teadawgs-minions-project/src/app/add-post/page.tsx
+++ teadawgs-minions-project/src/app/add-post/page.tsx
@@ -165,7 +165,7 @@ export default function AddPost(){
                             </svg>
                             <span className="text-[#D8D8D8] text-lg">Drafts</span>
                         </button>
-                        <button className="flex justify-center items-center gap-2 bg-[#E54747] px-8 py-3 rounded-lg hover:bg-[#F55757] transition-colors w-[40%] ml-2" style={{marginRight: 'auto'}}>
+                        <button onClick={() => handleSubmit(false)} className="flex justify-center items-center gap-2 bg-[#E54747] px-8 py-3 rounded-lg hover:bg-[#F55757] transition-colors w-[40%] ml-2" style={{marginRight: 'auto'}}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#D8D8D8" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
                             </svg>
