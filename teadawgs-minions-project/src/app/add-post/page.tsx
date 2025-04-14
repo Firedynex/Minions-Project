9bebb247d566d041db2c03320e1ff13ed5c40269
peterbenbui <peterbui420@gmail.com>
2025-04-14 17:32:51 -0400

    form and debuggin

--- teadawgs-minions-project/src/app/add-post/page.tsx
+++ teadawgs-minions-project/src/app/add-post/page.tsx
@@ -27,7 +27,8 @@ export default function AddPost(){
         const{ name, value } = e.target;
         setFormData(prev => ({...prev, [name]:value}));
     };
-    const handleSubmit = (isDraft: boolean) => {
+    const handleSubmit = (e: FormEvent, isDraft: boolean) => {
+        e.preventDefault();
         const status = isDraft ? 'draft' : 'published';
         const newItem: Item = {
             ...formData,
@@ -79,6 +80,11 @@ export default function AddPost(){
                 ))
                 }
             </div>
+            <form onSubmit = {(e) => {
+                e.preventDefault();
+                handleSubmit(e, false);
+            }}
+            >
             <div 
                 className="absolute"
                 style={{
@@ -182,13 +188,13 @@ export default function AddPost(){
                         marginLeft: 'calc(40%)',
                         marginRight: '30px'
                     }}>
-                        <button onClick={() => handleSubmit(true)} className="flex justify-center items-center gap-2 bg-[#757575] px-8 py-3 rounded-lg hover:bg-[#858585] transition-colors w-[40%]">
+                        <button type="button" onClick={(e) => handleSubmit(e, true)} className="flex justify-center items-center gap-2 bg-[#757575] px-8 py-3 rounded-lg hover:bg-[#858585] transition-colors w-[40%]">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#D8D8D8" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
                             </svg>
                             <span className="text-[#D8D8D8] text-lg">Drafts</span>
                         </button>
-                        <button onClick={() => handleSubmit(false)} className="flex justify-center items-center gap-2 bg-[#E54747] px-8 py-3 rounded-lg hover:bg-[#F55757] transition-colors w-[40%] ml-2" style={{marginRight: 'auto'}}>
+                        <button type="submit" className="flex justify-center items-center gap-2 bg-[#E54747] px-8 py-3 rounded-lg hover:bg-[#F55757] transition-colors w-[40%] ml-2" style={{marginRight: 'auto'}}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#D8D8D8" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
                             </svg>
@@ -196,6 +202,7 @@ export default function AddPost(){
                         </button>
                     </div>
             </div>
+            </form>
             <div className="fixed inset-0 flex items-center" style={{pointerEvents: 'none'}}>
                 <div className="relative ml-[15px]"
                     style={{
