16f51f18298a3413dd1164db83c9e68c7612f54c
Firedynex <58797454+Firedynex@users.noreply.github.com>
2025-04-15 20:59:05 -0400

    Got app connected to database

--- teadawgs-minions-project/src/app/page.tsx
+++ teadawgs-minions-project/src/app/page.tsx
@@ -2,8 +2,10 @@
 import Header from "../components/ui-elements/Header";
 import { useState } from "react";
 import Splash from "@/components/Splash";
+import connectMongoDB from "../../config/mongodb";
 
 export default function Home() {
+  connectMongoDB();
   // Simulate authentication state
   const [authenticated, setAuthenticated] = useState(false);
   return (
