361c0962d999a8f62c9b726bf6bbacbd9a97f126
Kareem Coulibaly <105329840+KascarkPrime@users.noreply.github.com>
2025-04-23 01:59:42 -0400

    pulled main

--- teadawgs-minions-project/src/app/history/page.tsx
+++ teadawgs-minions-project/src/app/history/page.tsx
@@ -1,6 +1,5 @@
 import Sidebar from "@/components/ui-elements/Sidebar";
-import UserHistory from "@/components/ui-elements/History";
-
+import UserHistory from "@/components/ui-elements/history";
 export default function AuthenticatedHomePage() {
     return(
         <>
