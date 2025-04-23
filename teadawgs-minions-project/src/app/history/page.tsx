707dd4aaacfa38c0e6adf6cf51c7b1712a00556a
Kareem Coulibaly <105329840+KascarkPrime@users.noreply.github.com>
2025-04-22 20:56:33 -0400

    added more history page functionality

--- teadawgs-minions-project/src/app/history/page.tsx
+++ teadawgs-minions-project/src/app/history/page.tsx
@@ -1,26 +1,37 @@
-import { SessionProvider } from "next-auth/react"
-import Sidebar from "@/components/ui-elements/Sidebar";
-import UserHistory from "@/components/ui-elements/History";
+'use client';
+import { SessionProvider } from 'next-auth/react';
+import UserHistory from '@/components/ui-elements/history';
+import Sidebar from '@/components/ui-elements/Sidebar';
 
 export default function AuthenticatedHomePage() {
-    return (
-        <SessionProvider>
-            <br />
-            <div className="m-3 flex flex-row justify-center items-center"> 
-                <Sidebar />
-                <UserHistory />
-            </div>
+  return (
+    <SessionProvider>
+      <AuthenticatedContent />
+    </SessionProvider>
+  );
+}
 
-            {/* Bubbles */}
-            <div className="fixed bottom-0 left-0 -translate-x-3/8 translate-y-3/8 h-90 w-90 rounded-full bg-black z-[-1]"></div>
-            <div className="fixed bottom-30 left-0 -translate-x-5/8 h-50 w-50 rounded-full bg-black z-[-1]"></div>
-            <div className="fixed bottom-0 left-40 translate-y-3/4 h-50 w-50 rounded-full bg-black z-[-1]"></div>
-            
-            <div className="fixed top-0 right-0 translate-x-1/4 h-80 w-80 rounded-full bg-black z-[-1]"></div>
-            <div className="fixed top-45 right-0 translate-x-5/8 h-50 w-50 rounded-full bg-black z-[-1]"></div>
-            <div className="fixed top-0 right-40 -translate-y-1/4 h-50 w-50 rounded-full bg-black z-[-1]"></div>
-            <div className="fixed top-0 right-70 -translate-y-3/4 h-50 w-50 rounded-full bg-black z-[-1]"></div>
-            <div className="fixed top-0 right-0 -translate-y-1/4 h-50 w-50 bg-black z-[-1]"></div>
-        </SessionProvider>
-    );
+function AuthenticatedContent() {
+  return (
+    <>
+      <br />
+      <div className="m-3 flex flex-row justify-center items-center"> 
+        <Sidebar />
+        <UserHistory/>
+      </div>
+
+      {/* Bubbles background */}
+      {/* bottom-left */}
+      <div className="fixed bottom-0 left-0 -translate-x-3/8 translate-y-3/8 h-90 w-90 rounded-full bg-black z-[-1]"></div>
+      <div className="fixed bottom-30 left-0 -translate-x-5/8 h-50 w-50 rounded-full bg-black z-[-1]"></div>
+      <div className="fixed bottom-0 left-40 translate-y-3/4 h-50 w-50 rounded-full bg-black z-[-1]"></div>
+      
+      {/* top-right */}
+      <div className="fixed top-0 right-0 translate-x-1/4 h-80 w-80 rounded-full bg-black z-[-1]"></div>
+      <div className="fixed top-45 right-0 translate-x-5/8 h-50 w-50 rounded-full bg-black z-[-1]"></div>
+      <div className="fixed top-0 right-40 -translate-y-1/4 h-50 w-50 rounded-full bg-black z-[-1]"></div>
+      <div className="fixed top-0 right-70 -translate-y-3/4 h-50 w-50 rounded-full bg-black z-[-1]"></div>
+      <div className="fixed top-0 right-0 -translate-y-1/4 h-50 w-50 bg-black z-[-1]"></div>
+    </>
+  );
 }
